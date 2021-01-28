9a6c5537635547e08f6200acc5c4bad45b6ec33c
Amar Ćatović <amar.catovic@wbee.io>
2021-01-28 13:13:50 +0100

    Task 6: Implement interfaces

---
+++ News Backend/News.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+using News.Core.Interfaces.Repositories;
+using News.Core.Models.Domain;
+
+namespace News.Persistence.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Method that gets all categories from the database
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        /// <summary>
+        /// Method that returns single Category from database. It includes all news that are posted in that category.
+        /// </summary>
+        /// <param name="categoryId">Id of category</param>
+        /// <returns>Category object</returns>
+        public async Task<Category> GetCategoryByIdAsync(int categoryId)
+        {
+            return await _context.Categories
+                .Include(c => c.News)
+                .SingleOrDefaultAsync(c => c.CategoryId == categoryId);
+        }
+    }
+}
---
+++ News Backend/News.Persistence/Repositories/NewsRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+using News.Core.Interfaces.Repositories;
+
+namespace News.Persistence.Repositories
+{
+    public class NewsRepository : INewsRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NewsRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Gets all news from database. Includes User and Category objects.
+        /// </summary>
+        /// <returns>List of News objects</returns>
+        public async Task<IEnumerable<Core.Models.Domain.News>> GetAllNewsAsync()
+        {
+            return await _context.News
+                .Include(n => n.User)
+                .Include(n => n.Category)
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Gets single news from database. Includes User and Category objects.
+        /// </summary>
+        /// <param name="id">news id</param>
+        /// <returns>News object</returns>
+        public async Task<Core.Models.Domain.News> GetNewsByIdAsync(int id)
+        {
+            return await _context.News
+                .Include(n => n.User)
+                .Include(n => n.Category)
+                .SingleOrDefaultAsync(n => n.NewsId == id);
+        }
+
+        /// <summary>
+        /// Adds single news to the database.
+        /// </summary>
+        /// <param name="news">News object</param>
+        /// <returns>null if something went wrong, News object if okay</returns>
+        public async Task<Core.Models.Domain.News> AddNewsAsync(Core.Models.Domain.News news)
+        {
+            await _context.AddAsync(news);
+            if (await _context.SaveChangesAsync() < 0)
+                return null;
+
+            return news;
+        }
+
+        /// <summary>
+        /// Edits single news in the database.
+        /// </summary>
+        /// <param name="newsId">news id</param>
+        /// <param name="news">News object</param>
+        /// <returns>true if okay, false otherwise</returns>
+        public async Task<bool> EditNewsAsync(int newsId, Core.Models.Domain.News news)
+        {
+            var newsFromDb = await _context.News.SingleOrDefaultAsync(n => n.NewsId == newsId);
+            if (newsFromDb == null)
+                return false;
+
+            newsFromDb = news;
+
+            return (await _context.SaveChangesAsync() >= 0);
+        }
+    }
+}
