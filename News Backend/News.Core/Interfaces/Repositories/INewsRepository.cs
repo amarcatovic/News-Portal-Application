1e18590aca4b944e465f6448b48f762dbabafa52
Amar Ćatović <amar.catovic@wbee.io>
2021-01-28 12:57:43 +0100

    Task 5: Add Additional methods for Add and Edit

--- News Backend/News.Core/Interfaces/Repositories/INewsRepository.cs
+++ News Backend/News.Core/Interfaces/Repositories/INewsRepository.cs
@@ -17,5 +17,19 @@ namespace News.Core.Interfaces.Repositories
         /// <param name="id">id of searched news</param>
         /// <returns>News object</returns>
         Task<Models.Domain.News> GetNewsByIdAsync(int id);
+
+        /// <summary>
+        /// Method that adds news to database
+        /// </summary>
+        /// <param name="news">News object</param>
+        /// <returns>News object</returns>
+        Task<Models.Domain.News> AddNewsAsync(Models.Domain.News news);
+
+        /// <summary>
+        /// Method that edits single news in database
+        /// </summary>
+        /// <param name="news">News object</param>
+        /// <returns>true if edit is success, false otherwise</returns>
+        Task<bool> EditNewsAsync(Models.Domain.News news);
     }
 }
