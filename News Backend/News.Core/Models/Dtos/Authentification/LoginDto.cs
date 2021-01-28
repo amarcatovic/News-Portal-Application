1cbb43aca542a84bf26ebc4c21adf3f56d338272
Amar Ćatović <amar.catovic@wbee.io>
2021-01-28 15:31:30 +0100

    Add Required

--- News Backend/News.Core/Models/Dtos/Authentification/LoginDto.cs
+++ News Backend/News.Core/Models/Dtos/Authentification/LoginDto.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace News.Core.Models.Authentification
 {
     public class LoginDto
     {
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string PasswordRaw { get; set; }
     }
 }
--- News Backend/News.Core/Models/Dtos/Authentification/LoginResponseDto.cs
+++ News Backend/News.Core/Models/Dtos/Authentification/LoginResponseDto.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using News.Core.Models.Dtos.User;
 
 namespace News.Core.Models.Authentification
 {
     public class LoginResponseDto
     {
         public string Token { get; set; }
+
+        public UserReadDto User { get; set; }
     }
 }
