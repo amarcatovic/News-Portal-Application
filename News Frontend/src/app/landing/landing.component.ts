176297c07e01d01dee8277da5e433a3ba32f3aaf
Amar Ćatović <amar.catovic@wbee.io>
2021-01-29 12:30:41 +0100

    Edit landing (news) page

--- News Frontend/src/app/landing/landing.component.ts
+++ News Frontend/src/app/landing/landing.component.ts
@@ -1,4 +1,10 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { News } from './../shared/models/News';
+import { NewsService } from './../shared/services/news.service';
+import { CookieService } from 'ngx-cookie-service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/models/User';
+import { CategoriesService } from '../shared/services/categories.service';
 
 @Component({
     selector: 'app-landing',
@@ -10,8 +16,45 @@ export class LandingComponent implements OnInit {
   focus: any;
   focus1: any;
 
-  constructor() { }
+  public newsList: News[] = [];
 
-  ngOnInit() {}
+  public user: User = null;
+  public searchTerm: string = '';
 
+  public categoryId: number;
+
+  constructor(private cookieService: CookieService,
+    private newsService: NewsService,
+    private categoriesService: CategoriesService,
+    private route: ActivatedRoute,
+    private router: Router,) { }
+
+  ngOnInit() {
+    if(this.cookieService.check('user')){
+      this.user = JSON.parse(this.cookieService.get('user'));
+    }
+
+    this.route.paramMap.subscribe(params => {
+      this.categoryId = this.route.snapshot.params['categoryId'];
+      if(this.categoryId){
+        this.categoriesService.getCategoryWithNews(this.categoryId).subscribe(response => {
+          this.newsList = response.news;
+        });
+      }
+      else{
+        this.newsService.getAllNews().subscribe(response => {
+          this.newsList = response;
+        });
+      }
+    });
+  }
+
+  /**
+   * Sets news varable in news service and redirects user to edit news
+   * @param news - News Object
+   */
+  public editNews(news: News){
+    this.newsService.setNews(news);
+    this.router.navigate(['/news/add-edit/edit']);
+  }
 }
