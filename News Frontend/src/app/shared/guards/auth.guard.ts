c0a60a815821d58ac94bc87424e437c1f28186bf
Amar Ćatović <amar.catovic@wbee.io>
2021-01-29 12:36:58 +0100

    Implement CanActivate AuthGuard

---
+++ News Frontend/src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private cookieService: CookieService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+    return this.cookieService.check('token') && this.cookieService.check('user');
+  }
+  
+}
